aa2f2784bb185e190ebb76cfa9b6c735f6eef1a4
Moisés <moises@buclecreativo.com>
2018-10-03 19:34:25 +0200

    Finished

--- src/pages/book-details/book-details.ts
+++ src/pages/book-details/book-details.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BooksProvider, Book } from "../../providers/books/books";
 import { MessagesService } from '../../services/Messages';
+import { HomePage } from '../home/home';
+
 
 
 @Component({
@@ -18,7 +20,8 @@ export class BookDetailsPage implements OnInit {
             private formBuilder: FormBuilder,
             private navParam: NavParams,
             private booksProvider: BooksProvider,
-            private messagesService: MessagesService) {
+            private messagesService: MessagesService,
+            private alertCtrl: AlertController) {
   }
 
   ngOnInit(){
@@ -35,12 +38,36 @@ export class BookDetailsPage implements OnInit {
       }
   }
 
-  formSubmitted(){  
+  formSubmitted(){
     this.submitting = true  
     this.booksProvider.addBook(this.form.value).subscribe(() => {
       this.messagesService.newToast('New book added!')
       this.submitting = false
     })
+  }
+
+  deleteBook() {
+    this.alertCtrl.create({
+      title: 'Ei',
+      subTitle: 'Vas a borrar algo',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Go',
+        handler: () => {
+          this.booksProvider.deleteBook(this.book.id).subscribe(item => {
+            this.navCtrl.setRoot(HomePage)
+          })
+        }
+      }]
+    })
+    .present()
+
   }
 
 }
