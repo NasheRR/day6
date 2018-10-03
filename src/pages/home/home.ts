95fee331fffe3a8fe4792aa484d0994493126c5b
marqu3z8 <fjmpl5@gmail.com>
2018-10-03 18:54:27 +0200

    Update modificado home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BooksProvider, Book } from '../../providers/books/books';
+import { Subscription } from 'rxjs/Subscription';
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  items: Book[];
+  public suscription: Subscription;
 
+  constructor(public navCtrl: NavController,
+    public bookS: BooksProvider) {
+
+  }
+
+  ngOnInit(){
+    this.suscription = this.bookS.getBooks().subscribe((data: Book[])=>{
+      this.items = data;
+      console.log(data);
+
+    },(error)=>{
+        console.log(error);
+    });
   }
 
 }
