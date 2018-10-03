2e38e097075596987acc5d7b4f9aed3b0f290cda
Juan Nuñez <juanancon@gmail.com>
2018-10-03 18:35:44 +0200

    Update: Service books updated

--- src/providers/books/books.ts
+++ src/providers/books/books.ts
@@ -1,6 +1,6 @@
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 
 export interface Book {
   id?: number,
@@ -16,10 +16,33 @@ export class BooksProvider {
 
   public books: Book[];
 
-  public urlLocal: string = 'http://9a0427b5.ngrok.io/';
+  public urlLocal: string = 'http://9a0427b5.ngrok.io';
 
   constructor(public http: HttpClient) {
     console.log('Hello BooksProvider Provider');
   }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get(`${this.urlLocal}/books`) as Observable<Book[]>;
+  }
+
+  updateBook(id: number, body): Observable<Book[]> {
+    let params = body;
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+
+    return this.http.put( `${this.urlLocal}/books/${id}`, params, { headers: headers }) as Observable<Book[]>;
+  }
+
+  addBook(body): Observable<Book[]> {
+    let params = body;
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+    return this.http.post( `${this.urlLocal}/books`, params, { headers: headers }) as Observable<Book[]>;
+  }
+
+  deleteBook(id: number): Observable<Book[]> {
+    return this.http.delete( `${this.urlLocal}/books/${id}`) as Observable<Book[]>;
+  }
+
+
 
 }
